7286d7a3f3381fb36128465cc0a4832496146594
Petra Číhalová <pcihalov@redhat.com>
2024-01-15 22:40:46 +0100

    added path for GitHub PR list file and GitHub token into config file

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
+import os
 from pathlib import Path
 
 SERVICES_LINKS_EXAMPLE_PATH = Path("data/services_links_example.yml")
 SERVICES_LINKS_PATH = Path("data/services_links.yml")
+
+GITHUB_PR_LIST = Path("data/github_pr_list.json")
+
+GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
