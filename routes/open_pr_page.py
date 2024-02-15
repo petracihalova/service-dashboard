4f4c5b368b3ca355eb47af0ae38db1a6f9b9c60a
Petra Číhalová <pcihalov@redhat.com>
2024-02-15 22:39:19 +0100

    fixup! created page with datatable for open pull requests

--- routes/open_pr_page.py
+++ routes/open_pr_page.py
@@ -5,10 +5,8 @@ import github_api
 
 
 def get_github_open_pr(reload_data):
-    if not config.GITHUB_PR_LIST.is_file() or reload_data:
-        github_api.get_open_pull_request()
-
-    with open(config.GITHUB_PR_LIST, mode="r", encoding="utf-8") as file:
-        github_open_pr = json.load(file)
-
-    return github_open_pr
+    if config.GITHUB_PR_LIST.is_file() and not reload_data:
+        with open(config.GITHUB_PR_LIST, mode="r", encoding="utf-8") as file:
+            return json.load(file)
+    
+    return github_api.get_open_pull_request()
