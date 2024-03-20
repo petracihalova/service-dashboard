d9f37517bfa042002849ce0d8daf7c5c82a8a9d3
Petra Číhalová <pcihalov@redhat.com>
2024-03-20 09:07:21 +0100

    moved the script for update button into separate file

---
+++ static/js/update_button.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Function to create a spinner element
+    function createSpinner() {
+        var spinner = document.createElement("span");
+        spinner.className = "spinner-border spinner-border-sm";
+        spinner.setAttribute("role", "status");
+        spinner.setAttribute("aria-hidden", "true");
+        spinner.style.marginRight = "10px";
+        return spinner;
+    }
+
+    var updateButton = document.getElementById("update_button");
+    // Add event listener to the button click event
+    updateButton.addEventListener("click", function (e) {
+        this.textContent = "";
+        this.appendChild(createSpinner());
+        this.appendChild(document.createTextNode("Loading"));
+        this.disabled = true;
+        window.location.href = this.dataset.url;
+    });
+});
