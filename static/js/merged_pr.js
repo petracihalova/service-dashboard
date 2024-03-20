96fd9685af77755885591a98a028798845fea7d4
Petra Číhalová <pcihalov@redhat.com>
2024-03-20 09:04:24 +0100

    added the data-url attribute and update the script for the update button

--- static/js/merged_pr.js
+++ static/js/merged_pr.js
@@ -1,18 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var updateButton = document.getElementById("update_button");
-
-    // Function to set Update button text to "Loading" and get the spinner visible
-    function setLoadingState() {
-        updateButton.textContent = "";
-        updateButton.appendChild(createSpinner());
-        updateButton.appendChild(document.createTextNode("Loading"));
-        updateButton.disabled = true;
-
-        // Construct the URL with the parameter and navigate to it
-        var url = "merged_pr?reload_data=1";
-        window.location.href = url;
-    }
-
     // Function to create a spinner element
     function createSpinner() {
         var spinner = document.createElement("span");
@@ -23,9 +9,14 @@ document.addEventListener('DOMContentLoaded', function () {
         return spinner;
     }
 
-    // Attach the setLoadingState function to the button click event
-    updateButton.addEventListener("click", () => {
-        setLoadingState();
+    var updateButton = document.getElementById("update_button");
+    // Add event listener to the button click event
+    updateButton.addEventListener("click", function (e) {
+        this.textContent = "";
+        this.appendChild(createSpinner());
+        this.appendChild(document.createTextNode("Loading"));
+        this.disabled = true;
+        window.location.href = this.dataset.url;
     });
 });
 
