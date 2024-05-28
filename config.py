fa6f6eeb99572564b6cb713346f75cb886c42cc9
Petra Číhalová <pcihalov@redhat.com>
2024-05-28 16:24:10 +0200

    add logic for displaying the test data
    when app is started for the first time and custom data doesn't exist,
    the app will copy and display the test data

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 from pathlib import Path
 
-SERVICES_LINKS_EXAMPLE_PATH = Path("data/services_links_example.yml")
-SERVICES_LINKS_PATH = Path("data/services_links.yml")
+DATA_PATH_FOLDER = "./data"
+SERVICES_LINKS_PATH = Path(f"{DATA_PATH_FOLDER}/services_links.yml")
 
-GITHUB_PR_LIST = Path("data/github_pr_list.json")
+GITHUB_PR_LIST = Path(f"{DATA_PATH_FOLDER}/github_pr_list.json")
 GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
 GITHUB_PATTERN = (
     r"(?:https?://)?(?:www\.)?github\.com/(?P<owner>[\w-]+)/(?P<name>[\w-]+)/?"
 )
-GITHUB_MERGED_PR_LIST = Path("data/github_merged_pr_list.json")
+GITHUB_MERGED_PR_LIST = Path(f"{DATA_PATH_FOLDER}/github_merged_pr_list.json")
 
-GITLAB_PR_LIST = Path("data/gitlab_pr_list.json")
+GITLAB_PR_LIST = Path(f"{DATA_PATH_FOLDER}/gitlab_pr_list.json")
 GITLAB_TOKEN = os.environ.get("GITLAB_TOKEN")
 # all https://gitlab.cee.redhat.com repos exl "app-interface"
 GITLAB_PATTERN = r"(?:https?://)?(?:www\.)?gitlab\.cee\.redhat\.com/(?P<owner>[\w-]+)/(?!app-interface)(?P<name>[\w-]+)/?"
--- routes/open_pr_page.py
+++ routes/open_pr_page.py
@@ -9,19 +9,27 @@ def get_github_open_pr(reload_data):
     """
     Get GitHub open pull requests from a file or download new data.
     """
-    if config.GITHUB_PR_LIST.is_file() and not reload_data:
-        with open(config.GITHUB_PR_LIST, mode="r", encoding="utf-8") as file:
-            return json.load(file)
+    if config.GITHUB_TOKEN:
+        if not config.GITHUB_PR_LIST.is_file() or reload_data:
+            return github_api.get_open_pull_request()
+    else:
+        if not config.GITHUB_PR_LIST.is_file():
+            return {}
 
-    return github_api.get_open_pull_request()
+    with open(config.GITHUB_PR_LIST, mode="r", encoding="utf-8") as file:
+        return json.load(file)
 
 
 def get_gitlab_open_pr(reload_data):
     """
     Get GitLub open pull requests from a file or download new data.
     """
-    if config.GITLAB_PR_LIST.is_file() and not reload_data:
-        with open(config.GITLAB_PR_LIST, mode="r", encoding="utf-8") as file:
-            return json.load(file)
+    if config.GITLAB_TOKEN:
+        if not config.GITLAB_PR_LIST.is_file() or reload_data:
+            return gitlab_api.get_open_pull_request()
+    else:
+        if not config.GITLAB_PR_LIST.is_file():
+            return {}
 
-    return gitlab_api.get_open_pull_request()
+    with open(config.GITLAB_PR_LIST, mode="r", encoding="utf-8") as file:
+        return json.load(file)
