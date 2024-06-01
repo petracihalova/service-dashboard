86e6b3ffd67cc96ac7cd8ae075f9276d967be462
Petra Číhalová <pcihalov@redhat.com>
2024-06-01 12:33:39 +0200

    return empty dict if GITHUB_TOKEN and the file with merged list are missing

--- routes/merged_pr_page.py
+++ routes/merged_pr_page.py
@@ -11,6 +11,9 @@ def get_github_merged_pr(reload_data):
     if config.GITHUB_TOKEN:
         if not config.GITHUB_MERGED_PR_LIST.is_file() or reload_data:
             return github_api.get_merged_pull_request()
+    else:
+        if not config.GITHUB_MERGED_PR_LIST.is_file():
+            return {}
 
     with open(config.GITHUB_MERGED_PR_LIST, mode="r", encoding="utf-8") as file:
         return json.load(file)
