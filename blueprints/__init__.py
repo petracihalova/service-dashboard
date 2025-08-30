a770f940b2cc16cb3c5e5fefc6a679bba0c4c9da
Petra Číhalová <pcihalov@redhat.com>
2025-08-30 02:25:45 +0200

    add page with personal statistics

--- blueprints/__init__.py
+++ blueprints/__init__.py
@@ -6,6 +6,7 @@ from .deployments import (
 )
 from .jira_tickets import jira_tickets_bp
 from .overview import get_services_links, overview_bp
+from .personal_stats import personal_stats_bp
 from .pull_requests import pull_requests_bp
 from .release_notes import release_notes_bp
 
@@ -14,6 +15,7 @@ __all__ = [
     "deployments_bp",
     "get_services_links",
     "overview_bp",
+    "personal_stats_bp",
     "pull_requests_bp",
     "get_stage_commit_style",
     "get_default_branch_commit_style",
